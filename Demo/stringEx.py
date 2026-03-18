51d34b307153a6124fba8971b7427a58b4bac4c1
unknown <08250269.sce@rub.gov.bt>
2026-03-18 09:53:51 +0530

    My First Python code

---
+++ Demo/stringEx.py
@@ -0,0 +1,11 @@
+print("hello")
+print("Hello")
+multipleLines = """This is a string that
+ spans multiple lines. It can be used to create paragraphs or to include line breaks in a string."""
+print(multipleLines) 
+a=1
+b=2
+c=a+b
+print(type(c))
+c= str(c)
+print(type(c))

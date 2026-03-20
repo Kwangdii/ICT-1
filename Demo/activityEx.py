bc797af68ba878a10078229acf1435c194fc26c8
unknown <08250269.sce@rub.gov.bt>
2026-03-20 16:20:51 +0530

    first class activity

---
+++ Demo/activityEx.py
@@ -0,0 +1,29 @@
+print("Welcome to my program dear users")
+print()
+name = input("Enter your name:")
+print()
+
+num1 = float(input("Enter the first number: "))
+print()
+
+num2 = float(input("Enter the second number: "))
+print()
+
+sum = num1 + num2 
+print("The sum of the two number is: ",sum)
+print()
+
+sub = num1 - num2
+print("The difference of the two number is:",sub)
+print()
+
+mul = num1 * num2
+print("The product of the two number is:", mul)
+print()
+
+div = num1 / num2
+print("The quotient of the two number is:",div)
+print()
+
+print("Thank you for your response!! ", name)
+print()

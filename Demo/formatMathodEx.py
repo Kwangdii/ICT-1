3b0967dca19fb51b4a9efc534e09cae343266c0f
unknown <08250269.sce@rub.gov.bt>
2026-03-25 10:25:10 +0530

    trying out the format method where we will use {} instead of modulo %

---
+++ Demo/formatMathodEx.py
@@ -0,0 +1,6 @@
+print()
+print("we are in unit {}".format(2))
+print()
+
+print("{p} is a programming language. With the help of {p} we can create powerful applications.".format(p = 'Python'))
+print()
